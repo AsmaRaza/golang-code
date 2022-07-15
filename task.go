9d7ea01739fa4810983f600a0a23473585c6684a
AsmaRaza <asmaraza5100@gmail.com>
2022-07-15 11:24:47 +0500

    channel go routines

--- task.go
+++ task.go
@@ -1,52 +1,77 @@
 package main
 
 import (
+	//"flag"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"time"
 )
 
+func runningtime(s string) (string, time.Time) {
+	log.Println("Start:	", s)
+	return s, time.Now()
+}
+
+func track(s string, startTime time.Time) {
+	endTime := time.Now()
+	log.Println("End:	", s, "took", endTime.Sub(startTime))
+}
+
+func execute() {
+	defer track(runningtime("execute"))
+	time.Sleep(3 * time.Second)
+}
+
 //function for counting characters from textfile
-func CountCharacters(data string) {
+func CountCharacters(data string, c chan string) {
 	c_count := 0
 	for i := 0; i < len(data); i++ {
 		c_count++
 	}
-	fmt.Println("The number of characters in a file =", c_count)
+
+	result1 := fmt.Sprintln("The number of characters in a file is =", c_count)
+	c <- result1
 
 }
 
 //function for counting words from text file
-func CountWord(data string) {
+func CountWord(data string, c chan string) {
 	w_count := 0
 	for i := 0; i < len(data); i++ {
 		if data[i] == ' ' {
 			w_count++
+
 		}
-		// count = count + 1
+
 	}
-	fmt.Println("The number of  words in a file =", w_count)
+
+	result2 := fmt.Sprintln("The number of words in a file is =", w_count)
+	c <- result2
 
 }
 
 //function for counting lines from textfile
-func CountLines(data string) {
+func CountLines(data string, c chan string) {
 	l_count := 0
 	for i := 0; i < len(data); i++ {
 		if data[i] == '\n' {
 			l_count++
-			//i++
+
 		}
 
+		result3 := fmt.Sprintln("The number of lines in a file is =", l_count)
+		c <- result3
+		l_count++
+
 	}
-	l_count++
-	fmt.Println("The number of lines in a file =", l_count)
 
 }
 
 //function for counting whitespaces in a textfile
 
-func CountWhiteSpaces(data string) {
+func CountWhiteSpaces(data string, c chan string) {
 	ws_count := 0
 	for i := 0; i < len(data); i++ {
 		if data[i] == ' ' {
@@ -54,10 +79,14 @@ func CountWhiteSpaces(data string) {
 		}
 
 	}
-	fmt.Println("The number of whitespaces in a file is =", ws_count)
+	result4 := fmt.Sprintln("The number of whitespaces in a file is =", ws_count)
+	c <- result4
 }
 
 func main() {
+	c := make(chan string)
+
+	execute()
 
 	var path string
 
@@ -65,21 +94,29 @@ func main() {
 	flag.Parse()
 	fmt.Println("Path", path)
 
-	// fmt.Println(path, "file.txt")
+	fmt.Println(path, "file.txt")
 
-	data, err := ioutil.ReadFile(path)
+	data, err := ioutil.ReadFile("file.txt")
 	//reading a file
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
 	// fmt.Println("Contents of file:")
-	// fmt.Println(string(data))
+	//fmt.Println(string(data))
+	//go final(string(data))
+	//w, z := <-c, <-c // receive from c
 
-	CountCharacters(string(data))
-	CountWord(string(data))
-	CountLines(string(data))
+	go CountCharacters(string(data), c)
+	go CountWord(string(data), c)
+	go CountLines(string(data), c)
 
-	CountWhiteSpaces(string(data))
+	go CountWhiteSpaces(string(data), c)
+	w, x := <-c, <-c
+	y, z := <-c, <-c
+	fmt.Println(w)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 
 }
